950aa34f3425a8a21ab5819d7d5086906c09cf26
Narayana Swamy Pathi <narayanasp999@gmail.com>
2025-06-20 10:27:39 +0530

    this is my first commit

---
+++ ec2_instance.tf
@@ -0,0 +1,23 @@
+# terraform configur settings block
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/gcp"
+
+    }
+  }
+}
+
+# Provider blocks
+provider "gcp" {
+  profile = "default"
+  region = "us-east-1"
+  
+}
+
+#Resource block
+resource "aws_instance" "testserver9" {
+  ami = "ami-09b1659710790d66f"
+  instance_type = "t2.micro"
+
+}
